bed5a81a17f1b75de84ba73b1814794b3a2d50fa
Decricher <55853487+pavelb22@users.noreply.github.com>
2022-01-14 22:40:32 +0300

    сделал менюшку

--- WINNER.py
+++ WINNER.py
@@ -10,10 +10,9 @@ def blue_win(blue):
                 and (i[0] + 2, i[1]) in blue\
                 and (i[0] + 3, i[1]) in blue:
             move_blue_x += 1
-            print(12)
         if move_blue_x == 2:
-            print('Синий выйграл!')
-            return
+            return True
+
     for i in blue:
         if (i[0], i[1] + 1) in blue\
                 and (i[0], i[1] + 2) in blue\
@@ -22,17 +21,16 @@ def blue_win(blue):
             move_blue_y += 1
             print(33)
         if move_blue_y == 2:
-            print('Синий выйграл!')
-            return
+            return True
+
     for i in blue:
         if (i[0] + 1, i[1] + 1) in blue\
                 and (i[0] + 2, i[1] + 2) in blue\
                 and (i[0] + 3, i[1] + 3) in blue:
             move_blue_diagonal += 1
-            print(22)
         if move_blue_diagonal == 2:
-            print('Синий выйграл!')
-            return
+            return True
+    return False
 
 def yellow_win(yellow):
     move_yellow_x = 1
@@ -47,23 +45,22 @@ def yellow_win(yellow):
 
             move_yellow_x += 1
         if move_yellow_x == 4:
-            print('Желтый выйграл!')
-            return
+            return True
+
     for i in yellow:
         if (i[0], i[1] + 1) in yellow\
                 and (i[0], i[1] + 2) in yellow\
                 and (i[0], i[1] + 3) in yellow:
             move_yellow_y += 1
         if move_yellow_y == 2:
-            print('Желтый выйграл!')
-            return
+            return True
     for i in yellow:
         if (i[0] + 1, i[1] + 1) in yellow and (i[0] + 2, i[1] + 2) in yellow and (i[0] + 3, i[1] + 3) in yellow:
 
             move_yellow_diagonal += 1
         if move_yellow_diagonal == 2:
-            print('Желтый выйграл!')
-            return
+            return True
+    return False
 
 
 def get_win(list_move):
@@ -77,8 +74,10 @@ def get_win(list_move):
             yellow.append(i)
         else:
             blue.append(i)
-    blue_win(blue)
-    yellow_win(yellow)
+    if blue_win(blue):
+        return 'Blue'
+    elif yellow_win(yellow):
+        return 'Yellow'
             # break
     # print(blue)
     # print(yellow)
@@ -86,3 +85,4 @@ def get_win(list_move):
 # [(6, 0), (4, 0), (6, 1), (3, 0), (5, 0), (2, 0), (6, 2), (3, 1)]
 #
 # get_win([(6, 0), (4, 0), (6, 1), (3, 0), (6, 2), (2, 0), (5, 0), (3, 1)])
+
--- main.py
+++ main.py
@@ -5,33 +5,57 @@ import sys
 from WINNER import get_win
 from table import Board
 from boal import BoalGame
+pygame.init()
 screen = pygame.display.set_mode((600, 500))
+running = True
+image_fon_load = pygame.image.load("start.jpg")
+image_fon = pygame.transform.scale(image_fon_load, (700, 700))
+play_load = pygame.image.load("eee.png")
+play_load.set_colorkey('WHITE')
+play_fon = pygame.transform.scale(play_load, (50, 50)).convert_alpha()
+screen.blit(image_fon, (0, 0))
+screen.blit(play_fon, (325, 145))
+f1 = pygame.font.Font(None, 55)
+play = f1.render('Play', 80, pygame.Color('#F23454'))
+screen.blit(play, (240, 150))
+pygame.display.flip()
+
 image = pygame.image.load("a.jpg")
 image1 = pygame.transform.scale(image, (700, 700))
-screen.blit(image1, (0, 0))
 board = Board(7, 6, image1)
-running = True
 board.coord()
 
 boal = BoalGame(7, 6, image1)
 boal.coord()
 
-
+fon = True
 board.render(screen)
-pygame.display.flip()
-
+boal.drow_static(screen)
 while running:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            coord = board.on_click(board.get_cell(event.pos))
-            # print(board.get_cell(event.pos))
-            # print(boal.list)
-            if boal.flag:
-                print(boal.list_move)
-                boal.drow_ball(screen, coord)
-                get_win(boal.list_move)
-            pygame.display.flip()
+            if fon is False:
+                screen.blit(image1, (0, 0))
 
+                pygame.display.flip()
+                coord = board.on_click(board.get_cell(event.pos))
+                # print(board.get_cell(event.pos))
+                # print(boal.list)
+                if boal.flag:
+                    boal.drow_ball(screen, coord)
+                    print(get_win(boal.list_move))
+                    boal.drow_static(screen)
 
+                pygame.display.flip()
+            else:
+                if event.pos[0] > 240 and event.pos[0] < 375 and event.pos[1] > 150 and event.pos[1] < 200:
+                    screen.blit(image1, (0, 0))
+                    boal.render(screen)
+                    pygame.display.flip()
+
+                    fon = False
+
+        # fon = False
